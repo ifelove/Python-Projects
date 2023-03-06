29db78d84107bd18f8375c03e17013421a042cd6
Ifeoluwa <ifel1939@gmail.com>
2023-03-06 22:45:01 +0100

    Tittactoe game with no graphics

---
+++ tictactoe-no-graphics/main.py
@@ -0,0 +1,27 @@
+import math
+import random
+
+class Player:
+    def _init_(self,letter):
+        # letter is 'x or 'o'
+        self.letter=letter
+
+    def getMove(self,game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def _ini_(self,letter):
+        super()._init_(letter)
+
+    def getMove(self,game):
+        pass
+
+class HumanPlayer(Player):
+    def _ini_(self,letter):
+        super()._init_(letter)
+
+    def getMove(self,game):
+        pass
+
+
